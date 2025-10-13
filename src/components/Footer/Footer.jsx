5005c071c2046b4d92b0673d3cc0dd0811419b8c
JuriJuriM <jurimudry@gmail.com>
2025-10-13 11:16:16 +0300

    my first comment

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer.scss'
+
+const Footer = () => {
+    return (
+        <div className='footer'>
+            <div className='footer__body'>
+                <div className='footer__title'>
+                    Все права защищены &copy;
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import './Header.scss'
+import Burger from './menu.svg.png'
+import BurgerClose from './close-menu.png'
+import Logo from '../../assets/img/avatar.jpg'
+import Main from '../pages/Main.jsx'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+
+const Header = () => {
+    const [isOpen, setOpen] = useState();
+    return (
+        <>
+            <header className='header'>
+                <div className='container'>
+                    <div className='header__body'>
+                        <div className='header__logo'>
+                            <img src={Logo} alt='Logo' />
+                        </div>
+                        <nav className={`header__nav ${isOpen ? 'active' : ''}`}>
+                            <ul className='header__nav-list'>
+                                <li className='header__nav-item'>
+                                    <a href='/'>Main</a>
+                                </li>
+                            </ul>
+                        </nav>
+                        <span className='header__menu-button' onClick={() => setOpen(!isOpen)}>
+                            {isOpen ? <img src={BurgerClose} alt='menu-close' /> : <img src={Burger} alt='burger' />}
+                        </span>
+                    </div>
+                </div>
+            </header>
+            <Router className='router'>
+                <Routes>
+                    <Route path='/' element={<Main />} />
+                </Routes>
+            </Router>
+        </>
+
+    )
+}
+
+export default Header
