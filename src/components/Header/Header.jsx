058d36e2baa9b9344df858da73817de5bb8b2503
JuriJuriM <jurimudry@gmail.com>
2025-10-13 11:48:30 +0300

    Main

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
                         <nav className={`header__nav ${isOpen ? 'active' : ''}`}>
                             <ul className='header__nav-list'>
                                 <li className='header__nav-item'>
-                                    <Link to='/'>Main</Link>
+                                    <Link to='/'>Главная</Link>
                                 </li>
                             </ul>
                         </nav>
